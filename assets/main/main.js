4484ba2c4da7ba54d6f5bafdb5439bef06f7a614
Duy Toan <toanpmo123@gmail.com>
2021-10-24 20:59:30 +0700

    update header + banner

---
+++ assets/main/main.js
@@ -0,0 +1,19 @@
+let formpop = {
+    run: function() {
+        this.show()
+        this.close()
+    },
+    show: function() {
+        let btnPopup = document.querySelector('.btn-popup')
+        let formPopup = document.querySelector('.popup-form')
+        btnPopup.addEventListener('click', () => {
+            formPopup.classList.add('show')
+        })
+    },
+    close: function() {
+        let closePopup = document.querySelector('.popup-close')
+        let formPopup = document.querySelector('.popup-form')
+        closePopup.addEventListener('click', () => {
+            formPopup.classList.remove('show')
+        })
+    }
